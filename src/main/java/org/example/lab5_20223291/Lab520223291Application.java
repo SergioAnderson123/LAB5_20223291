74e556741e868ace73650aeb71ff156e5be40abc
Sergio Meneses Ramos <a20223291@pucp.edu.pe>
2026-05-07 19:46:28 -0500

    proyecto subido

---
+++ src/main/java/org/example/lab5_20223291/Lab520223291Application.java
@@ -0,0 +1,13 @@
+package org.example.lab5_20223291;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab520223291Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab520223291Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lab5_20223291/Lab520223291ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab5_20223291;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab520223291ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
